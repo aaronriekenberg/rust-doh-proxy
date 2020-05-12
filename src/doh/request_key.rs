aa056ef381b8c89192cea6e2204095576f4d220b
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2020-05-12 17:28:51 -0500

    Use Vec of objects inside RequestKey.

--- src/doh/request_key.rs
+++ src/doh/request_key.rs
@@ -1,35 +1,42 @@
 use trust_dns_proto::op::Message;
+use trust_dns_proto::rr::dns_class::DNSClass;
+use trust_dns_proto::rr::record_type::RecordType;
 
 use std::convert::TryFrom;
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct RequestQueryKey {
+    name: String,
+    query_type: RecordType,
+    query_class: DNSClass,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct RequestKey {
-    key: String,
+    query_keys: Vec<RequestQueryKey>,
 }
 
 impl TryFrom<&Message> for RequestKey {
     type Error = &'static str;
 
     fn try_from(message: &Message) -> Result<Self, Self::Error> {
-        let mut first = true;
-        let mut key = String::new();
+        let mut query_keys = Vec::with_capacity(message.queries().len());
 
         for query in message.queries() {
-            if !first {
-                key.push('|');
-            }
-            key.push_str(&query.name().to_string().to_lowercase());
-            key.push(':');
-            key.push_str(&u16::from(query.query_type()).to_string());
-            key.push(':');
-            key.push_str(&u16::from(query.query_class()).to_string());
-            first = false;
+            let mut name_string = query.name().to_string();
+            name_string.make_ascii_lowercase();
+
+            query_keys.push(RequestQueryKey {
+                name: name_string,
+                query_type: query.query_type(),
+                query_class: query.query_class(),
+            });
         }
 
-        if key.is_empty() {
-            Err("key string is empty")
+        if query_keys.is_empty() {
+            Err("query_keys is empty")
         } else {
-            Ok(RequestKey { key })
+            Ok(RequestKey { query_keys })
         }
     }
 }
